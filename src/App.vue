af914797a11f184bc4cad0c8d6a3061104912f1a
Kevinfal <kevinfal383@gmail.com>
2024-05-24 11:57:14 +0300

    Ajout des fonction supprimer , supprimerTous et evaluerExpression. Le projet est fonctionnel.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
-  <h1>CALCULATRICE 🤓</h1>
+  <h1>CALCULATRICE 🤔🤓</h1>
   <div class="corps">
     <div class="calcul">
       <p>{{ calcul }}</p>
     </div>
     <div class="clavier">
       <div class="nombre">
-          <div class="nbr" v-for="n in [1,2,3,4,5,6,7,8,9,0]" :key="n">
+          <div class="nbr" v-for="n in [1,2,3,4,5,6,7,8,9,0,'.']" :key="n">
             <button @click="ajouterNombre(n)">{{ n }}</button>
           </div>
         </div>
       <div class="operateur">
         <div class="delete">
-          <button>Del</button>
+          <button @click="supprimer()">Del</button>
+        </div>
+        <div class="delete">
+          <button @click="supprimerTous()">C</button>
         </div>
         <div class="op" v-for="op in ['+', '-', '*', '/', '%', '=']" :key="op">
           <button @click="ajouterOperateur(op)">{{ op }}</button>
@@ -39,7 +42,38 @@
         }
       },
       ajouterOperateur(op){
-        this.calcul = this.calcul + String(op)
+        if (op === '='){
+          this.evaluerExpession();
+        } 
+        else {
+          this.calcul = this.calcul + op;
+        }
+      },
+      /*evaluerExpession(){
+        try {
+        // Utilisation de Function constructor au lieu de eval() pour évaluer l'expression
+        this.calcul = String(Function('"use strict";return (' + this.calcul + ')')());
+      } catch (e) {
+        this.calcul = "Erreur";
+      }
+      }*/
+      //La fonction eval en JavaScript évalue une chaîne de caractères comme du code JavaScript et retourne le résultat de cette évaluation
+      evaluerExpession(){
+        try {
+          this.calcul = String(eval(this.calcul));
+        } catch (e) {
+          this.calcul = "Syntaxe erreur";
+        }
+      },
+      supprimer(){
+        if (this.calcul.length > 1) {
+          this.calcul = this.calcul.slice(0, -1).trim();
+        } else {
+          this.calcul = "0";
+        }
+      },
+      supprimerTous(){
+        this.calcul = "0";
       }
     }
   }
@@ -71,7 +105,6 @@
       width: 400px;
       margin: 0px auto;
       button{
-        font-size: 40px;
         background: #000;
         border: none;
         color: white;
@@ -84,15 +117,21 @@
           border: 3px solid white;
           text-align: center;
           padding-top: 24px;
+          button{
+            font-size: 40px;
+          }
         }
       }
       .operateur{
         display: grid;
-        grid-template: repeat(7, 57.1px) / repeat(1, 100px);
+        grid-template: repeat(7, 50px) / repeat(1, 100px);
         .op , .delete{
           border: 3px solid white;
           text-align: center;
-          padding-top: 2px;
+          button{
+            padding-top: 5px;
+            font-size: 30px;
+          }
         }
       }
     } 
