fdecde81b0a0a1c5759479da01510cfc103cc7f1
Kevinfal <kevinfal383@gmail.com>
2024-05-24 09:11:00 +0300

    Le commencement du projet. J'ai creé l'interface du projet.Puis deux methode ajouterNombre et ajouterOperateur

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,100 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>CALCULATRICE 🤓</h1>
+  <div class="corps">
+    <div class="calcul">
+      <p>{{ calcul }}</p>
+    </div>
+    <div class="clavier">
+      <div class="nombre">
+          <div class="nbr" v-for="n in [1,2,3,4,5,6,7,8,9,0]" :key="n">
+            <button @click="ajouterNombre(n)">{{ n }}</button>
+          </div>
+        </div>
+      <div class="operateur">
+        <div class="delete">
+          <button>Del</button>
+        </div>
+        <div class="op" v-for="op in ['+', '-', '*', '/', '%', '=']" :key="op">
+          <button @click="ajouterOperateur(op)">{{ op }}</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<script>
+  export default{
+    data(){
+      return{
+        calcul: "0",
+      }
+    },
+    methods:{
+      ajouterNombre(n){
+        if (this.calcul === "0"){
+          this.calcul = String(n)
+        } else {
+          this.calcul = this.calcul + String(n)
+        }
+      },
+      ajouterOperateur(op){
+        this.calcul = this.calcul + String(op)
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style lang="scss">
+  body{
+    font-family: sans-serif;
+    background: #000;
+    color: white;
+    h1{
+      font-size: 35px;
+      text-align: center;
+    }
+    .calcul{
+      border: 5px solid white;
+      width: 400px;
+      margin: 20px auto 0px;
+      height: 100px;
+      p{
+        font-size: 40px;
+        padding-left: 8px;
+      }
+    }
+    .clavier{
+      display: flex;
+      border: 5px solid white;
+      width: 400px;
+      margin: 0px auto;
+      button{
+        font-size: 40px;
+        background: #000;
+        border: none;
+        color: white;
+      }
+      .nombre{
+        display: grid;
+        /*4 ligne de 30px et 3 colone de 30px*/ 
+        grid-template: repeat(4, 100px) / repeat(3, 100px);
+        .nbr{
+          border: 3px solid white;
+          text-align: center;
+          padding-top: 24px;
+        }
+      }
+      .operateur{
+        display: grid;
+        grid-template: repeat(7, 57.1px) / repeat(1, 100px);
+        .op , .delete{
+          border: 3px solid white;
+          text-align: center;
+          padding-top: 2px;
+        }
+      }
+    } 
+  }
 </style>
